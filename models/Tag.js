0555193d6de66d34c23df9e7e67a38ff3307254e
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-06-26 15:56:16 -0700

    update seed data

--- models/Tag.js
+++ models/Tag.js
@@ -1,29 +1,29 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require("../config/connection.js");
+const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true,
-		},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
     tag_name: {
-      	type: DataTypes.STRING,
+      type: DataTypes.STRING,
+      allowNull: false
     }
-		// define columns
-	},
-	{
-		sequelize,
-		timestamps: false,
-		freezeTableName: true,
-		underscored: true,
-		modelName: "tag",
-	}
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
 );
 
 module.exports = Tag;
--- models/index.js
+++ models/index.js
@@ -1,31 +1,29 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  as: 'category',
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  as: 'products',
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    foreignKey: 'product_id'
-  }
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
 })
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through:{
-    model: ProductTag,
-    foreignKey: 'tag_id'
-  }
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id'
 })
-
 
 module.exports = {
   Product,
