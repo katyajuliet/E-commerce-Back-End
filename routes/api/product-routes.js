4f3e94d712e255da07f4dcc3b078ea301c8aa55d
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-04 22:40:38 -0700

    updated routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -73,7 +73,7 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    //category?
+    
   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
