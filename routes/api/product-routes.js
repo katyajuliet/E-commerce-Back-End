cefcf6ecb4d9d4a3e0445bafb796c9a22e42aaa3
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-04 23:19:53 -0700

    update minor spacing and testing delete route issue

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -124,12 +124,12 @@ router.put('/:id', (req, res) => {
             tag_id,
           };
         });
-      // figure out which ones to remove
+      
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
 
-      // run both actions
+
       return Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
